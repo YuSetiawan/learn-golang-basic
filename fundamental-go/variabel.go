c2d8c913f08d1bab7d0e4860fecfaf58654f4e9d
work.ysetiawan <work.ysetiawan@gmail.com>
2025-07-15 11:21:56 +0700

    variabel dalam golang

---
+++ fundamental-go/variabel.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func greet() {
+
+	// variabel nama pertama
+	var firstName string = "yusuf"
+
+	// variabel nama terakhir
+	var lastName string
+	lastName = "setiawan"
+
+	fmt.Printf("halo %s %s!\n", firstName, lastName)
+}
